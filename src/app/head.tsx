f2765ce1a59073576cafdcb8d18a42573723f3fb
Moshe_Pvdick <bilmike1543@gmail.com>
2026-02-02 14:56:21 +0100

    Use Century Gothic sitewide
    Set global typography to Century Gothic and drop Lora.

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,16 +4,6 @@ export default function Head() {
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link
-        rel="preconnect"
-        href="https://fonts.gstatic.com"
-        crossOrigin="anonymous"
-      />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Lora:wght@400;500;600&display=swap"
-        rel="stylesheet"
-      />
       <link
         rel="preload"
         as="image"
